9deb2fcd2537720d8e414fa0de469f6388016174
jiyeeeah <sije3757@snu.ac.kr>
2023-11-20 11:35:01 +0900

    Fix: 모바일 위아래 잘림 현상 수정 9차

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="h-screen overflow-scroll snap snap-y snap-mandatory">
+    <div className="overflow-scroll h-real-screen snap snap-y snap-mandatory">
       <div className="flex flex-col items-center justify-center flex-shrink-0 w-full h-real-screen snap-start">
         <img src={GraduationRecitalUrl} alt="Graduation Recital" className="p-3" />
         <img src={mainImageUrl} alt="메인사진" />
